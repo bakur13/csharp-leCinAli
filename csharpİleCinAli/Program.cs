e94d865d2ec3f2e6e5d7134fb6574b7e50f43764
Hamza Uslu <108797599+bakur13@users.noreply.github.com>
2023-02-23 13:38:38 +0300

    Update Program.cs

--- csharpİleCinAli/Program.cs
+++ csharpİleCinAli/Program.cs
@@ -1,30 +1,89 @@
-using System;
-
-class Program
+char[,] dotMatris = new char[60, 60];
+for (int i = 0; i < dotMatris.GetLength(0); i++)
 {
-    static void Main(string[] args)
+    for (int j = 0; j < dotMatris.GetLength(1); j++)
     {
-        
-      
-        int r = 8;
+        dotMatris[i, j] = ' ';
+    }
+}
+//Kafa çizimi
 
-        // Çemberin merkezi, x = r ve y = r koordinatında olsun
-        for (int y = 0; y <= 2 * r; y++)
+KafaCiz();
+GovdeCizimi();
+Uzuvlar(0);
+/*
+ * 30,10
+ * 40,20
+ * 30,30
+ * 20,20
+ */
+
+
+void KafaCiz(int x = 10, int y = 10)
+{
+    int sayac1 = 0;
+    for (int i = 10; i >= -10; i -= 2)
+    {
+        int sayac2 = 20;
+        for (int j = -10; j <= 10; j++)
         {
-            for (int x = 0; x <= 2 * r; x++)
+            int hesap = (int)Math.Sqrt(Math.Pow(i, 2) + Math.Pow(j, 2));
+            if (hesap == 10)
             {
-                // Eğer (x - r)² + (y - r)² <= r² ise, o nokta çemberin içindedir
-                // Ayrıca, (x - r)² + (y - r)² >= (r - 1)² ise, o nokta da çemberin içindedir
-                if ((x - r) * (x - r) + (y - r) * (y - r) <= r * r && (x - r) * (x - r) + (y - r) * (y - r) >= (r - 1) * (r - 1))
-                {
-                    Console.Write("*");
-                }
-                else
-                {
-                    Console.Write(" ");
-                }
+                dotMatris[sayac1, sayac2] = '.';
             }
-            Console.WriteLine();
+
+            sayac2++;
         }
+        sayac1++;
+    }
+}
+
+//Gövde çizimi
+void GovdeCizimi()
+{
+    for (int i = 0; i < 15; i++)
+    {
+        dotMatris[10 + i, 30] = '.';
+    }
+}
+
+void Uzuvlar(int param)
+{
+    //sağ kol
+    for (int i = 0; i < 10; i++)
+    {
+        dotMatris[10 + i, 30 + i] = '.';
+    }
+    //sol kol
+    for (int i = 0; i < 10; i++)
+    {
+        dotMatris[10 + i, 30 - i] = '.';
+    }
+    //sağ bacak
+    for (int i = 0; i < 10; i++)
+    {
+        dotMatris[25 + i, 30 + i] = '.';
+    }
+    //sol bacak
+    for (int i = 0; i < 10; i++)
+    {
+        dotMatris[25 + i, 30 - i] = '.';
+    }
+}
+
+EkranaYaz();
+
+//Ekrana Yaz
+void EkranaYaz(bool isClear = true)
+{
+    if (isClear) Console.Clear();
+    for (int i = 0; i < dotMatris.GetLength(0); i++)
+    {
+        for (int j = 0; j < dotMatris.GetLength(1); j++)
+        {
+            Console.Write(dotMatris[i, j]);
+        }
+        Console.WriteLine();
     }
 }
